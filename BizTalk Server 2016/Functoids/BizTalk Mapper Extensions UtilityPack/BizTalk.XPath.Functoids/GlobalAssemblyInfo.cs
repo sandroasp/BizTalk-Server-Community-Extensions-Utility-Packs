27abed941d4d092f4387ef8fa93c8aeec56c80ac
Sandro Pereira <sandro-pereira@live.com.pt>
2018-02-28 22:14:57 +0000

    Add files via upload

---
+++ BizTalk Server 2016/Functoids/BizTalk Mapper Extensions UtilityPack/BizTalk.XPath.Functoids/GlobalAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("ValueBlue B.V.")]
+[assembly: AssemblyCopyright("Copyright © 2014 - Martijn Schiedon")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
